60ba1de4edd6c94aee8cd49c38a2b00c078b031f
Tomasz Czerniewski <brydlo2.tc@gmail.com>
2023-11-09 16:46:54 +0100

    Kolejna aktualizacja klasy ProductEndpoint

--- src/main/java/sklep/rest/ProductEndpoint.java
+++ src/main/java/sklep/rest/ProductEndpoint.java
@@ -1,13 +1,19 @@
 package sklep.rest;
-import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.server.ResponseStatusException;
 import sklep.model.Product;
 import sklep.repository.ProductRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/products")
 public class ProductEndpoint {
     private ProductRepository productRepository;
 
@@ -15,10 +21,22 @@ public class ProductEndpoint {
         this.productRepository = productRepository;
     }
 
-    @GetMapping("/products")
+    @GetMapping
     public List<Product> getAllProducts() {
-        System.out.println("puk puk");
         return productRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Product getOneProduct(@PathVariable("id") Integer id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        //Można też tak :
+//        Optional<Product> product = productRepository.findById(id);
+//        if(product.isPresent()) {
+//            return product.get();
+//        } else {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//        }
     }
 
 }
