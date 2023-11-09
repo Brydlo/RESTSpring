5e98ba057e142f7da1e17feb19b715b0cbe57376
Tomasz Czerniewski <brydlo2.tc@gmail.com>
2023-11-09 16:09:35 +0100

    Pierwszy commit

---
+++ src/main/java/com/example/tcrestspring/TcRestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tcrestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcRestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tcrestspring/TcRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tcrestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
