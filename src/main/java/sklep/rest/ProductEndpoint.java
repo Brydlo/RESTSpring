571825ae6b8d216b1a359a1f3d0678b78af00c8f
Tomasz Czerniewski <brydlo2.tc@gmail.com>
2023-11-09 16:21:48 +0100

    Wstrzykiwanie interfejsu przez konstruktor

--- src/main/java/sklep/rest/ProductEndpoint.java
+++ src/main/java/sklep/rest/ProductEndpoint.java
@@ -1,4 +1,19 @@
 package sklep.rest;
 
+import sklep.model.Product;
+import sklep.repository.ProductRepository;
+
+import java.util.List;
+
 public class ProductEndpoint {
+
+    private ProductRepository productRepository;
+
+    public ProductEndpoint(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
